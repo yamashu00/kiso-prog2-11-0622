302c8ce71529439303614cac69e94b0dc699487d
新矢結菜 <arayayuna@shinyayuinanonotobukkukonpyuta.local>
2026-06-22 14:56:05 +0900

    Clear Task

--- ex_read.c
+++ ex_read.c
@@ -52,7 +52,7 @@ int main(void) {
              *
              *   引数: data.location, data.temperature, data.humidity
              */
-            printf("まだ表示できていません\n"); /* TODO 1: この行を直す */
+            printf("%s: %dC / %.1f%%\n", data.location, data.temperature, data.humidity); /* TODO 1: この行を直す */
 
             count++;
         }
@@ -66,6 +66,8 @@ int main(void) {
      */
     /* TODO 2: ここに printf を1行書く */
 
+
+    printf("合計：%d 件\n", count);
     fclose(fp);
     fp = NULL;
     return 0;
